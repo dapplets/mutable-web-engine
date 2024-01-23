f2e83fd17e559d709490aa79385765e111b2c210
Lisofffa <lisofffa@gmail.com>
2024-01-24 02:22:35 +0300

    feat: added json-parser test

---
+++ test/core/parsers/json-parser.test.ts
@@ -0,0 +1,81 @@
+import { InsertionType } from "../../../src/core/adapters/interface";
+import { IParser } from "../../../src/core/parsers/interface";
+import { JsonParser } from "../../../src/core/parsers/json-parser";
+import { IContextNode } from "../../../src/core/tree/types";
+
+const config = {
+  namespace: "sampleNamespace",
+  contexts: {
+    root: {
+      selector: ".context1-selector",
+      props: {
+        id: "string('root')",
+      },
+      children: ["post", "profile"],
+      insertionPoints: {
+        insertionPoint1: {
+          selector: ".insertion-point-selector",
+          bosLayoutManager: "layoutManager1",
+          insertionType: InsertionType.After,
+        },
+        insertionPoint2: "data-insertion-point",
+      },
+    },
+    post: {
+      insertionPoints: {
+        insertionPoint1: {
+          selector: ".insertion-point-selector",
+          bosLayoutManager: "layoutManager1",
+          insertionType: InsertionType.After,
+        },
+        insertionPoint2: "data-insertion-point",
+      },
+    },
+    profile: {},
+  },
+};
+
+describe("JSON parser", () => {
+  let element: HTMLElement;
+  let ns: string;
+  let jsonParser: IParser;
+  let ctx: IContextNode;
+
+  beforeEach(() => {
+    element = document.createElement("div");
+    element.innerHTML = `  <div id="root">
+    <div class="context1-selector" id='test' data-context="context1">
+      <p data-prop1="value1" data-prop2="value2">Context 1 Content</p>
+      <div data-insertion-point="insertionPoint1" class="insertion-point-selector">
+        Insertion Point 1 Content
+      </div>
+      <div data-insertion-point="insertionPoint2">
+        Insertion Point 2 Content
+      </div>
+      <div data-child="child1">Child 1 Content</div>
+      <div data-child="child2">Child 2 Content</div>
+    </div>
+  </div>`;
+
+    ns = "https://dapplets.org/ns/json/some-web-site";
+    jsonParser = new JsonParser(config);
+    ctx = jsonParser.parseContext(element, "root");
+  });
+
+  it("should return a parsed context", () => {
+    console.log(ctx);
+    expect(ctx.id).toBe("root");
+  });
+
+  it("should return a child", () => {
+    expect(jsonParser.findChildElements(element, "post"));
+  });
+
+  it("should find insertionPoint", () => {
+    expect(jsonParser.findInsertionPoint(element, "root", "insertionPoint2"));
+  });
+
+  it("should return insertionPoints", () => {
+    expect(jsonParser.getInsertionPoints(element, "root"));
+  });
+});
