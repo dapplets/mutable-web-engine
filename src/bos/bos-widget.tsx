7e5dd08675bb314b494606a62cc8a742e79a0160
Alexander Sakhaev <alsakhaev@gmail.com>
2024-05-15 13:50:02 +0300

    fix: injected widgets float on the top

--- src/bos/bos-widget.tsx
+++ src/bos/bos-widget.tsx
@@ -86,7 +86,6 @@ export class BosComponent extends HTMLElement {
         justify-content: center;
         position: relative;
         visibility: visible !important;
-        z-index: 999999;
       }
     `
     this._adapterStylesMountPoint.innerHTML = resetCssRules
