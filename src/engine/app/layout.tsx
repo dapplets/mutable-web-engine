f21687fd100fe7aa1dddd8f97d6dceef7ddbe747
Lisofffa <lisofffa@gmail.com>
2024-06-13 15:27:46 +0300

    fix: added notification after merge

--- src/engine/app/layout.tsx
+++ src/engine/app/layout.tsx
@@ -4,12 +4,14 @@ import { ContextPicker } from './components/context-picker'
 import { ContextManager } from './components/context-manager'
 import { ShadowDomWrapper } from '../bos/shadow-dom-wrapper'
 import { useEngine } from './contexts/engine-context'
+import { Notification } from './components/notification'
 
 export const Layout: FC = () => {
   const { viewportRef } = useEngine()
 
   return (
     <ShadowDomWrapper ref={viewportRef}>
+      <Notification />
       <ContextPicker />
       <ContextManager />
     </ShadowDomWrapper>
